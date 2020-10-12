f5c2120fbff59334b01f201deb2e71d53a9ce97e
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-11 23:18:31 -0300

    creating vars with let // var creation rules

---
+++ back-to-basics/aula07.js
@@ -0,0 +1,25 @@
+let nome;
+
+console.log(`${nome} nasceu em 1984`);
+console.log(`Em 2000 ${nome} conheceu Maria`);
+console.log(`${nome} casou-se com Maria em 2012`);
+console.log(`Maria teve 1 filho com ${nome} em 2015`);
+console.log(`O filho de ${nome} chama-se Eduardo`);
+
+// não podemos criar variáveis com palavras reservadas let, if, etc
+// variáveis precisam ter nomes significativos
+
+// não iniciar o nome de uma variável com números
+// let 1numero  = 1 // vai dar erro!
+let nome1 = 1;
+
+// ou usar camel case ou usar snake case
+let _numero_aleatorio = 3; //snake_case
+let numero_aleatorio = 9; // snake_case
+let numeroAleatorio = 5; // camelCase
+
+// variáveis são case-sensitive
+let numAleatorio = 5;
+let numaleatorio = 5;
+
+// não podemos redeclarar variáveis (let)
