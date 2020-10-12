13e8a3126dd7b944c3c285310d5287ea022a87b1
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-11 22:05:25 -0300

    console.log concat exercise

---
+++ back-to-basics/aula04.js
@@ -0,0 +1,12 @@
+// concatenando
+console.log(
+  'Meu nome é "Luciano". Estou aprendendo javascript no domingo as ' +
+    10 +
+    " horas da noite."
+);
+// usando vírgula
+console.log(
+  'Meu nome é "Luciano". Estou aprendendo javascript no domingo as',
+  10,
+  "horas da noite."
+);
