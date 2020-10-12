dd240693816eb21bd66bdbe89b65efc3a8044a94
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-11 21:58:02 -0300

    about console.log()

---
+++ back-to-basics/aula03.js
@@ -0,0 +1,6 @@
+console.log('Luciano dii Souza'); // String
+console.log("Luciano dii Souza"); // String
+console.log(`Luciano dii Souza`); // String
+
+console.log(123456) // integer OR number
+console.log(1.4) // decimal OR float (number)
