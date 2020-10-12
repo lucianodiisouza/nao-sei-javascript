2da7ae46d7128b96a9c0df962ee33c5cf8820761
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-11 22:17:04 -0300

    comments in js

---
+++ back-to-basics/aula05.js
@@ -0,0 +1,8 @@
+// Comentários
+
+// Este é um comentário de uma linha
+console.log("Olá mundo!"); // comentário na frente da linha de código
+/* 
+comentário multilinha ou 
+comentário de bloco
+*/
