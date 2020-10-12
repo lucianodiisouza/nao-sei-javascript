491c3cc6cd4767609015eade450c0c9ee8d1fc3f
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-12 11:53:57 -0300

    constants

---
+++ back-to-basics/aula08.js
@@ -0,0 +1,20 @@
+// Não podemos criar constantes com nomes reservados
+// Constantes precisam ter nomes significativos
+// Não podemos começar o nome de uma constante com um número
+// Não podemos colocar espaços ou traços
+// Devemos usar camelCase (std) ou snake_case
+// Case-sensitive
+// Não modificar o valor de uma constante
+// Não utilizar var, utilizar const
+
+// const nome = "João";
+// console.log(nome);
+
+// const soma = primeiroNum + segundoNum;
+// const somaDuplicada = soma * 2;
+// let somaTriplicada = soma * 3;
+// somaTriplicada = somaTriplicada + 5;
+
+const primeiroNum = true;
+const segundoNum = 2;
+console.log(primeiroNum + segundoNum);
